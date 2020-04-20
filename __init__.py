f6fc24706e81334ffc32d1a9730b460647291e4a
Ryan Fobel <ryan@fobel.net>
2020-04-20 17:11:17 -0400

    Use __module__ instead of name

--- __init__.py
+++ __init__.py
@@ -4,10 +4,10 @@ import uasyncio as asyncio
 
 class App(BaseApp):
     # Setup
-    def __init__(self, name):
-        super().__init__(name)
+    def __init__(self):
+        super().__init__()
 
     # This function runs continuously
     async def loop(self):
-        print('[%s] state=%s' % (self.name, self._state))
+        print('[%s] state=%s' % (self.__module__, self._state))
         await asyncio.sleep(10)
